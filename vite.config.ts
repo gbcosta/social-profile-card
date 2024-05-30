948e5240a565ea8a43cd323a3f62b423fea811b3
gbcostasant@gmail.com <gbcostasant@gmail.com>
2024-05-30 18:39:13 -0300

    update build script

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import autoprefixer from "autoprefixer"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build:{
+        outDir: "build"
+    },
     css:{
         postcss:{
             plugins:[tailwind(), autoprefixer()]
