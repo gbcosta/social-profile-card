679a04130b2571dc4b390285bc5995dbaa8877e7
gbcostasant@gmail.com <gbcostasant@gmail.com>
2024-05-30 03:41:40 -0300

    change profile image to CardHeader from Card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,10 +1,8 @@
 import CardHeader from "./cardHeader";
 import CardBody from "./cardBody";
-import image from "../assets/img.png";
 
 const Card: React.FunctionComponent = () =>{
     return <div className="flex flex-col bg-zinc-800 gap-4 rounded-lg p-8">
-        <img src={image} className="w-32 rounded-full self-center"/>
         <CardHeader />
         <CardBody />
     </div>
--- src/components/cardHeader.tsx
+++ src/components/cardHeader.tsx
@@ -1,8 +1,11 @@
+import image from "../assets/img.png";
+
 const CardHeader: React.FunctionComponent = () =>{
-    return <div className="text-center">
+    return <div className="text-center flex flex-col items-center">
+        <img src={image} className="w-32 rounded-full"/>
         <h1 className="text-white font-bold text-xl">Gabriel Santos</h1>
-        <h2 className="text-blue-500 font-bold text-lg">Brazil</h2>
-        <p className="text-white">"Full Stack Developer from Brazil"</p>
+        <h2 className="text-green-400 font-bold text-lg">Brazil</h2>
+        <p className="text-white">Full Stack Developer and vim user</p>
     </div>
 }
 
