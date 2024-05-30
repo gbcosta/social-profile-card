1c67a6e7d296c518276e08f1d3a1d99527168f24
gbcostasant@gmail.com <gbcostasant@gmail.com>
2024-05-30 03:39:04 -0300

    added profile image to the card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,10 @@
 import CardHeader from "./cardHeader";
 import CardBody from "./cardBody";
+import image from "../assets/img.png";
 
 const Card: React.FunctionComponent = () =>{
-    return <div className="flex flex-col bg-zinc-800 gap-4 rounded-lg p-4">
+    return <div className="flex flex-col bg-zinc-800 gap-4 rounded-lg p-8">
+        <img src={image} className="w-32 rounded-full self-center"/>
         <CardHeader />
         <CardBody />
     </div>
